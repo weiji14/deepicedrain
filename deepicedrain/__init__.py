02904bb4dd4ea9774daf187b06502482c94461c3
Wei Ji <weiji.leong@vuw.ac.nz>
2020-05-29 10:54:03 +1200

    :ambulance: Package atlas_catalog.yaml into deepicedrain
    So that calling `deepicedrain.catalog` will actually work when people `pip install deepicedrain` without cloning the git repository (otherwise a FileNotFoundError is raised). Added a plugin to pyproject.toml so that the ATLAS catalog can be loaded via intake through `intake.cat.atlas_cat` too! Done by moving atlas_catalog.yaml and tests/ into the deepicedrain folder.
    
    This relies on a bit of magic (good ones), using the Python 3.7+ importlib.resources module to locate the atlas_catalog.yaml file via a relative path where the package is installed (in site-packages). Basically following a modified version of https://github.com/intake/intake-examples/tree/04bbe1880f2a4d2c74c6ea9c54385c380c1b9a1e/data_package. Had to use {{ CATALOG_DIR }} to link to the test_catalog.yaml file too. Side effect of this is that we're bundling all our tests into the `deepicedrain` python package, which might be bad for file size but good for finding test examples I guess.

--- deepicedrain/__init__.py
+++ deepicedrain/__init__.py
@@ -1,10 +1,18 @@
+import importlib.resources
+import logging
+
 import intake
+
+import deepicedrain
 from deepicedrain.deltamath import calculate_delta
 from deepicedrain.spatiotemporal import Region
 
 __version__: str = "0.1.0"
 
 # Loads the ICESat-2 ATLAS intake data catalog
-catalog: intake.catalog.local.YAMLFileCatalog = intake.open_catalog(
-    uri="atlas_catalog.yaml"
+_catalog_path = importlib.resources.path(
+    package=deepicedrain, resource="atlas_catalog.yaml"
 )
+with _catalog_path as uri:
+    logging.info(f"Loading intake catalog from {uri}")
+    catalog: intake.catalog.local.YAMLFileCatalog = intake.open_catalog(uri=str(uri))
