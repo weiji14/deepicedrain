85f74a7d3824f057fe377843737b698f85f69d32
Wei Ji <weiji.leong@vuw.ac.nz>
2020-12-19 14:17:45 +1300

    :clown_face: Mock up subglacial lake crossover parallel bdd runner
    Chucking lots of code from atlxi_xover.py into our Behavioural Driven Development integration test suite. Not very DRY, but will refactor later. Should work by running `poetry run pytest --verbose --doctest-modules -k anomalies -n 2 deepicedrain/`. Dropped all the lake_ids from all the examples in subglacial_lakes.feature as they're listed in atlas_catalog.yaml now, one source of truth! Also made the `df_lake` function into a reusable fixture in conftest.py, setting it up for shaing across the test_subglacial_lakes_*.py tests.

--- deepicedrain/tests/conftest.py
+++ deepicedrain/tests/conftest.py
@@ -1,7 +1,15 @@
 """
 This module contains shared fixtures, steps, and hooks.
 """
+import os
+
+import fsspec
+import numpy as np
+import pandas as pd
 import pytest
+from pytest_bdd import given
+
+import deepicedrain
 
 
 @pytest.fixture
@@ -14,3 +22,73 @@ def context():
         pass
 
     return Context()
+
+
+@pytest.fixture
+def client():
+    """
+    A dask distributed client to throw compute tasks at!
+    """
+    import dask
+
+    tag: str = "X2SYS"
+    os.environ["X2SYS_HOME"] = os.path.abspath(tag)
+    _client = dask.distributed.Client(
+        n_workers=8, threads_per_worker=1, env={"X2SYS_HOME": os.environ["X2SYS_HOME"]}
+    )
+
+    yield _client
+
+    _client.shutdown()
+
+
+@given("some altimetry data over <lake_name> at <location>", target_fixture="df_lake")
+def lake_altimetry_data(lake_name: str, location: str, context) -> pd.DataFrame:
+    """
+    Load up some pre-processed ICESat-2 ATL11 altimetry data from a Parquet
+    file and subset it to a specific lake region.
+    """
+    context.lake_name: str = lake_name
+    # TODO use intake_parquet after https://github.com/intake/intake-parquet/issues/18
+    with fsspec.open(
+        f"simplecache::https://github.com/weiji14/deepicedrain/releases/download/v0.3.1/df_dhdt_{location}.parquet",
+        simplecache=dict(cache_storage="ATLXI", same_names=True),
+    ) as openfile:
+        dataframe: pd.DataFrame = pd.read_parquet(openfile)
+
+    # Get lake outline from intake catalog
+    lake_catalog = deepicedrain.catalog.subglacial_lakes()
+    lake_ids: list = (
+        pd.json_normalize(lake_catalog.metadata["lakedict"])
+        .query("lakename == @lake_name")
+        .ids.iloc[0]
+    )
+    context.lake: pd.Series = (
+        lake_catalog.read()
+        .loc[lake_ids]
+        .dissolve(by=np.zeros(shape=len(lake_ids), dtype="int64"), as_index=False)
+        .squeeze()
+    )
+
+    # Contextual metadata on lake's bounding box region, and its drain/fill state
+    context.region = deepicedrain.Region.from_gdf(
+        gdf=context.lake, name=context.lake_name
+    )
+    context.draining: bool = True if context.lake.inner_dhdt < 0 else False
+
+    # Subset data to lake of interest
+    context.placename: str = context.lake_name.lower().replace(" ", "_")
+    df_lake: pd.DataFrame = context.region.subset(data=dataframe)
+
+    # Save lake outline to OGR GMT file format
+    os.makedirs(name=f"figures/{context.placename}", exist_ok=True)
+    context.outline_points: str = f"figures/{context.placename}/{context.placename}.gmt"
+    try:
+        os.remove(path=context.outline_points)
+    except FileNotFoundError:
+        pass
+    lake_catalog.read().loc[list(lake_ids)].to_file(
+        filename=context.outline_points, driver="OGR_GMT", mode="w"
+    )
+
+    return df_lake
