f785876c387b3372e4ef7d3cd27ede18562a5772
Wei Ji <23487320+weiji14@users.noreply.github.com>
2021-03-11 13:38:52 +1300

    :bookmark: v0.4.1
    Release v0.4.1.

--- deepicedrain/__init__.py
+++ deepicedrain/__init__.py
@@ -26,7 +26,7 @@ from deepicedrain.vizplots import (
     plot_icesurface,
 )
 
-__version__: str = "0.4.0"
+__version__: str = "0.4.1"
 
 # Loads the ICESat-2 ATLAS intake data catalog
 _catalog_path = importlib.resources.path(
